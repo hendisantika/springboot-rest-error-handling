48aff0de5ffbf9c18450ff076daac4ee0ba5f154
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-15 17:43:27 +0700

    #15 - Add newBook API in BookController

--- src/main/java/com/hendisantika/springbootresterrorhandling/controller/BookController.java
+++ src/main/java/com/hendisantika/springbootresterrorhandling/controller/BookController.java
@@ -5,8 +5,11 @@ import com.hendisantika.springbootresterrorhandling.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 /**
@@ -29,5 +32,11 @@ public class BookController {
     @GetMapping("/books")
     List<Book> findAll() {
         return bookRepository.findAll();
+    }
+
+    // Save
+    @PostMapping("/books")
+    Book newBook(@Valid @RequestBody Book newBook) {
+        return bookRepository.save(newBook);
     }
 }
