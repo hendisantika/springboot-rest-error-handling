56c4c0534c8474cdf0ff975b77356509f8aa43fd
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-15 17:32:07 +0700

    #4 - Add Book Entity

---
+++ src/main/java/com/hendisantika/springbootresterrorhandling/entity/Book.java
@@ -0,0 +1,49 @@
+package com.hendisantika.springbootresterrorhandling.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import java.math.BigDecimal;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-rest-error-handling
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 15/10/20
+ * Time: 17.25
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotEmpty(message = "Please provide a name")
+    private String name;
+
+    @NotEmpty(message = "Please provide a author")
+    private String author;
+
+    @NotBlank(message = "Please provide a price")
+    @DecimalMin("1.00")
+    private BigDecimal price;
+
+    public Book(String name, String author, BigDecimal price) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+}
