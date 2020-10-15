a15d4503aa8168ccc352376a5810f212a408a2d4
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-15 17:22:41 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootresterrorhandling/SpringbootRestErrorHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootresterrorhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestErrorHandlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRestErrorHandlingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootresterrorhandling/SpringbootRestErrorHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootresterrorhandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestErrorHandlingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
